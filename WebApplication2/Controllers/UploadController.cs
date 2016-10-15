d34ad220d561d174e508439ceac7115783adbe5b
a0919376604 <a0919376604@gmail.com>
2016-10-15 21:26:19 +0800

    完成版

---
+++ WebApplication2/Controllers/UploadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class UploadController : ApiController
+    {
+        [Route("api/Files/Upload")]
+        public async Task<string> Post()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Current.Request;
+
+                if(httpRequest.Files.Count > 0)
+                {
+                    foreach(string file in httpRequest.Files)
+                    {
+                        var postedFile = httpRequest.Files[file];
+
+                        var folderName = postedFile.FileName.Split('\\').FirstOrDefault();
+                        var fileName = postedFile.FileName.Split('\\').LastOrDefault().Split('/').LastOrDefault();
+
+                        var folderPath = HttpContext.Current.Server.MapPath("~/Uploads/" + folderName);
+                        var filePath = HttpContext.Current.Server.MapPath("~/Uploads/" + folderName+"/" + fileName);
+
+                        if (!Directory.Exists(folderPath))
+                        {
+                            Directory.CreateDirectory(folderPath);
+                        }
+                        postedFile.SaveAs(filePath);
+
+                        return "/Uploads/" + folderName + "/" + fileName;
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                return exception.Message;
+            }
+
+            return "No files";
+        }
+    }
+}
